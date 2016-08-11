15513ff7eae00312947a5ddf814218be2957b29a
Niall Smith <niallsmi@gmail.com>
2016-08-11 13:19:05 +0100

    forums

--- threads/admin.py
+++ threads/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Subject, Post, Thread
 
 # Register your models here.
+
+admin.site.register(Subject)
+admin.site.register(Post)
+admin.site.register(Thread)
--- threads/views.py
+++ threads/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from threads.models import Subject
 
 # Create your views here.
+
+def forum(request):
+    return render(request, 'forum.html', {'subjects':Subject.objects.all()})
+
+def threads(request, subject_id):
+    subject = get_object_or_404(Subject, pk=subject_id)
+    return render(request, 'threads.html', {'subject':subject})
