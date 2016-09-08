8a16d39433c4a94e6ae4ea23c9c4637174173094
Niall Smith <niallsmi@gmail.com>
2016-09-08 18:07:42 +0100

    Finished!

--- threads/templatetags/thread_extras.py
+++ threads/templatetags/thread_extras.py
@@ -33,9 +33,10 @@ def user_vote_button(thread, subject, user):
     if not vote:
         if user.is_authenticated():
             link = """
-            <div class="col-md-3 btn-vote">
-            <a href="%s" class="btn btn-default btn-sm">
-            Add my vote! </a>
+                <div class='col-md-3 btn-vote'>
+                <a href='%s' class='btn btn-default btn-sm'>
+                    Add my vote!
+                </a>
             </div>""" % reverse('cast_vote', kwargs={'thread_id' : thread.id, 'subject_id' : subject.id})
             return link
 
