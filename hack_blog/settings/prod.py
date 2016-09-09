aa535665801e347e9a77b7fd987198184b438ada
Niall Smith <niallsmi@gmail.com>
2016-09-09 09:19:44 +0100

    Finished! But won't run on Heroku

--- hack_blog/settings/prod.py
+++ hack_blog/settings/prod.py
@@ -3,8 +3,8 @@ import dj_database_url
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
